cb1476473ba3cdffac249e1bff4f5510f4d7c4fd
Pim Verlangen <pimverlangen@gmail.com>
2022-12-01 21:17:32 +0100

    Refactor reading of elves

--- day_one.go
+++ day_one.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func DayOneA() Elf {
+func CreateElfList() []Elf {
 	elves := make([]Elf, 0)
 	var elf = CreateElf()
 
@@ -31,35 +31,18 @@ func DayOneA() Elf {
 	sort.SliceStable(elves, func(i, j int) bool {
 		return elves[i].Totalcalories() > elves[j].Totalcalories()
 	})
+
+	return elves
+}
+
+func DayOneA() Elf {
+	elves := CreateElfList()
 
 	return elves[0]
 }
 
 func DayOneB() []Elf {
-	elves := make([]Elf, 0)
-	var elf = CreateElf()
-
-	onLineRead := func(line string) {
-		if line == "" {
-			elves = append(elves, elf)
-			elf = CreateElf()
-		} else {
-			calories, err := strconv.ParseInt(line, 10, 0)
-			if err != nil {
-				log.Print("Skipping elf, invalid calories")
-			} else {
-				elf.items = append(elf.items, ElfItem{
-					calories: calories,
-				})
-			}
-		}
-	}
-
-	ReadFile(onLineRead)
-
-	sort.SliceStable(elves, func(i, j int) bool {
-		return elves[i].Totalcalories() > elves[j].Totalcalories()
-	})
+	elves := CreateElfList()
 
 	return elves[0:3]
 }
